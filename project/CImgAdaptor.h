be83442c9c09f9f3580b79a5e2d338be491ff0bd
Vladislav <vladislavhacker111@gmail.com>
2021-04-14 19:44:38 +0300

    lodepng

---
+++ project/CImgAdaptor.h
@@ -0,0 +1,8 @@
+//
+// Created by vh on 14.04.21.
+//
+
+#ifndef INC_1C_CIMGADAPTOR_H
+#define INC_1C_CIMGADAPTOR_H
+
+#endif //INC_1C_CIMGADAPTOR_H
--- project/main.cpp
+++ project/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-#include "CImg/CImg.h"
+#include "lodepng.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<unsigned char> png;
+    std::vector<unsigned char> image; //the raw pixels
+    unsigned int width, height;
+
+    //load and decode
+    unsigned int error = lodepng::load_file(png, "/home/vh/Apply1C/input/image.png");
+    if (!error) {
+        error = lodepng::decode(image, width, height, png);
+    }
+
+    //if there's an error, display it
+    if (error) {
+        std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+    } else {
+        std::cout << width << " " << height;
+    }
+
+
+
     return 0;
 }
